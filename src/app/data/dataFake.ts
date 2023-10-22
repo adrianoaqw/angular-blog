2d03b125734d1cc03b66fb831f7199b09735f5d3
Adriano <adriano.sousaaaa@gmail.com>
2023-10-21 23:37:18 -0300

    :mag: Content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+      "id":"1",
+      "title": "NOVO HOMEM DE FERRO EM 3D",
+      "description": "Aliquam tincidunt velit a nisi ullamcorper luctus. Curabitur blandit egestas nisi, et porttitor dolor pharetra eget. In quis mauris tellus. Praesent faucibus pulvinar arcu sed viverra. Curabitur eget eros at orci venenatis porta et eu mauris. Suspendisse a velit et erat ultricies condimentum at sit amet ipsum. Praesent commodo mauris lobortis tortor luctus, vitae bibendum purus mattis. Nulla eu est diam. Curabitur vitae lobortis nibh. Etiam ac turpis eget eros suscipit aliquet nec a ipsum. Praesent nec magna iaculis, dapibus tortor ac, ornare neque. Sed ex nulla, posuere at risus non, pretium bibendum libero.",
+      "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+    {
+      "id":"2",
+      "title": "Nova Série anunciada no Disney +",
+      "description": "Donec non nulla id sem tempor lobortis. Pellentesque fermentum porta turpis vel ultricies. In sit amet erat gravida nunc rhoncus venenatis quis eget nibh. Ut a sapien ex. Phasellus iaculis tellus eget placerat pulvinar. Sed faucibus eu massa in vehicula. Suspendisse sit amet congue ante. Donec orci turpis, feugiat efficitur aliquet eget, aliquet porttitor lacus. Vestibulum malesuada felis aliquam nibh tempus venenatis.",
+      "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    },
+    {
+        "id":"3",
+        "title": "Novo filme do pantera negra lançado em breve",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus euismod leo eu porta. Aenean viverra accumsan eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In at ipsum ac lectus facilisis consequat. Maecenas libero sem, pellentesque vitae elit vel, rhoncus convallis enim. Duis eu nibh pulvinar, viverra mauris ut, cursus risus. Integer in nunc dolor. Aenean vel congue lectus. Sed erat ex, ornare mattis blandit blandit, ultricies sed massa. Nulla vitae ligula erat. Ut vitae turpis non magna tempus ultrices. Pellentesque a sollicitudin turpis. Nulla at convallis ex. Sed pellentesque placerat urna vel aliquet. Mauris sed egestas ante.",
+        "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+    {
+        "id":"4",
+        "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+        "description": "Cras pretium vestibulum diam at viverra. Phasellus gravida consequat nulla at pellentesque. Duis semper lorem at eros efficitur mollis. Nulla quis lacinia turpis. Phasellus diam eros, vulputate in fringilla non, porta eget metus. Curabitur pulvinar non neque in dignissim. Quisque efficitur, erat eu congue venenatis, velit enim auctor ante, vel sodales tellus enim non risus.",
+        "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+    }
+  ]
